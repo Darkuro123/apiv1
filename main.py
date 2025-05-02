1a5efa0d8a9d2a08a1946a4b4cc06fdf35310783
Darkuro123 <ramdows123@gmail.com>
2025-05-01 20:32:38 -0400

    desplieges

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from auth import createToken, validateToken
 from bd.database import engine, Base
 from routers.movie import routerMovie
 from routers.users import login_user
+import os
+import uvicorn
 
 app = FastAPI(
     title='Aprendiendo FastApi',
@@ -19,30 +21,11 @@ app.include_router(login_user)
 Base.metadata.create_all(bind=engine)
 
 
-moviess = [
-    {
-        "id": 3,
-        "title": "The Shawshank Redemption",
-        "overview": "Two imprisoned men bond over a number of years ...",
-        "year": 1994,
-        "rating": 9.3,
-        "category": "Drama"
+@app.get('/', tags=["Inicio"])
+def read_root():
+    return HTMLResponse('<h1>Bienvenido a FastAPI</h1>')
 
-    },
-    {
-        "id": 3,
-        "title": "The Godfather",
-        "overview": "An organized crime dynastys aging patriarch ...",
-        "year": 1972,
-        "rating": 9.2,
-        "category": "Drama"
-    },
-    {
-        "id": 3,
-        "title": "The Dark Knight",
-        'overview': 'When the menace known as the Joker emerges ...',
-        "year": 2008,
-        "rating": 9.0,
-        "category": "Action",
-    }
-]
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host='0.0.0.0', port=port)
